453438930ad3833295e5b51c09aec76e7145998f
Ashwin <apexashwin@gmail.com>
2023-04-30 23:32:03 +0300

    added search bar and aligned it below floating action button, will add the search functions later

--- lib/features/Index/index_screen.dart
+++ lib/features/Index/index_screen.dart
@@ -1,10 +1,10 @@
 import 'package:blueline_contacts/features/contacts/controller/contacts_controller.dart';
-import 'package:blueline_contacts/model/contact.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../core/widgets/bluelineappbar/controller/custom_appbar_controller.dart';
 import '../../core/widgets/bluelineappbar/custom_appbar.dart';
+import '../../model/contact.dart';
 import '../contacts/screen/contacts_listview_screen.dart';
 import '../contacts/screen/new_contact_screen.dart';
 
@@ -39,18 +39,54 @@ class Index extends ConsumerWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => NewContactScreen(
-                contact: Contact.init(),
+      floatingActionButton: Stack(
+        alignment: Alignment.bottomCenter,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 10),
+            child: SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width - 30,
+              child: TextFormField(
+                onTapOutside: (event) {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  label: const Text(
+                    'Search',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 1,
+                      color: Colors.blueGrey,
+                    ),
+                    borderRadius: BorderRadius.circular(70),
+                  ),
+                ),
               ),
             ),
-          );
-        },
-        child: const Icon(Icons.add),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NewContactScreen(
+                      contact: Contact.init(),
+                    ),
+                  ),
+                );
+              },
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ],
       ),
     );
   }
